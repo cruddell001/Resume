5fc363add1005bad6a143c58704c647efa8f84c3
chris <chris.ruddell@gmail.com>
2019-03-02 19:49:35 -0600

    Initial commit with base project and initial work on layout

---
+++ app/src/main/java/com/ruddell/resume/ui/TopLevelContentFragment.kt
@@ -0,0 +1,18 @@
+package com.ruddell.resume.ui
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.ruddell.resume.R
+
+class TopLevelContentFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_top_level_content, container, false)
+
+
+
+}
